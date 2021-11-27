5d9c5c7c4994076c880d96c3d6c0ca777a6576a7
Cristian <crist.lopezr@duocuc.cl>
2021-11-27 20:42:16 -0300

    agrega home

--- taller_Serviexpress/urls.py
+++ taller_Serviexpress/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import home
+from .views import home, listaServicios
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('listaServicios/',listaServicios, name='listaServicios')
 ]
--- taller_Serviexpress/views.py
+++ taller_Serviexpress/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"taller_Serviexpress/home.html")
+
+def listaServicios(request):
+    return render(request,"taller_Serviexpress/listaServicios.html")
